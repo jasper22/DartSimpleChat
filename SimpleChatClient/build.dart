82578ec4011cb7888798745a9650bf5e6f356b11
Jasper <jjasper22@gmail.com>
2013-04-23 14:50:06 +0300

    Cometic changes

--- SimpleChatClient/build.dart
+++ SimpleChatClient/build.dart
@@ -2,6 +2,16 @@ import 'dart:io';
 import 'package:web_ui/component_build.dart';
 
 // Ref: http://www.dartlang.org/articles/dart-web-components/tools.html
-main() {
-  build(new Options().arguments, ['web/index.html']);
+main()
+{
+  var args = new List.from(new Options().arguments);
+//  args.addAll(['--', '--no-rewrite-urls']);
+
+  build(args, ['web/index.html']);
+
+//  Future dwc = build(args, ['web/index.html'']);
+
+//  dwc
+//    .then((_) => Process.run('cp', ['packages/browser/dart.js', 'web/out/dart.js']))
+//    .then((_) => Process.run('cp', ['App.css', 'out']));
 }
